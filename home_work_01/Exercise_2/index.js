1c79b30b83fabe281ea66094f405359b8fbf34fa
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2023-09-27 12:43:33 +0300

    Initial commit

---
+++ home_work_01/Exercise_2/index.js
@@ -0,0 +1,37 @@
+/* Задание 2
+Напишите функцию createCounter, которая создает 
+счетчик и возвращает объект с двумя методами: 
+increment и decrement. Метод increment должен 
+увеличивать значение счетчика на 1, а метод 
+decrement должен уменьшать значение счетчика на 1. 
+Значение счетчика должно быть доступно только через 
+методы объекта, а не напрямую. */
+
+function createCounter() {
+    let counter = 0;
+    return {
+        increment() {
+            counter++;
+        },
+        decrement() {
+            counter--;
+        },
+        getValue() {
+            return counter;
+        }
+    };
+}
+
+
+const counter = createCounter();
+console.log(counter.getValue());
+counter.increment();
+console.log(counter.getValue());
+counter.increment();
+console.log(counter.getValue());
+counter.increment();
+console.log(counter.getValue());
+counter.increment();
+console.log(counter.getValue());
+counter.decrement();
+console.log(counter.getValue());
---
+++ home_work_01/Exercise_3/index.js
@@ -0,0 +1,31 @@
+/* Задание 3
+Дополнительное задание, выполняем только если 
+проходили работу с DOM. Напишите рекурсивную 
+функцию findElementByClass, которая принимает 
+корневой элемент дерева DOM и название класса в 
+качестве аргументов и возвращает первый найденный 
+элемент с указанным классом в этом дереве.
+
+Пример
+const rootElement = document.getElementById('root');
+const targetElement = findElementByClass(rootElement, 'my-class');
+console.log(targetElement); */
+
+function findElementByClass(element, className) {
+    if (element.classList && element.classList.contains(className)) {
+        return element;
+    }
+    const children = element.children;
+    for (let i = 0; i < children.length; i++) {
+        const foundElement = findElementByClass(children[i], className);
+        if (foundElement) {
+            return foundElement;
+        }
+    }
+    return null;
+}
+
+
+const rootElement = document.getElementById('root');
+const targetElement = findElementByClass(rootElement, 'my-class');
+console.log(targetElement);
